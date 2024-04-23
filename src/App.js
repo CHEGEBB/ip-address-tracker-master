0abd4cbe2011d23201ed17a22d54b5dd5138466e
CHEGEBB <chegephil24@gmail.com>
2024-04-23 10:07:00 +0300

    react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@
 function App() {
   return (
     <div className="App">
+    <div className="tracker">
+    <div className="header">
+      <h1>Ip Address Tracker</h1>
+      <div className="search">
+      <input type="text" placeholder="Search for any IP address or domain" />
+      </div>
+    </div>
+    <div className="map">
+      <div className="map-container">
+        <div id="map"></div>
+      </div>
+    </div>
+    </div>
     </div>
   );
 }
