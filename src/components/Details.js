afa140c1d1f83c103ff945918ce9fac22db12847
CHEGEBB <chegephil24@gmail.com>
2024-04-23 11:16:04 +0300

    scss

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,14 +8,17 @@ const DetailsComponent = () => {
                 <h2>Ip Address</h2>
                 <p>192.212.174.101</p>
             </div>
+            <div className="rectangle"></div>
             <div className="location">
                 <h2>Location</h2>
                 <p>Brooklyn, NY 10001</p>
                 </div>
+                <div className="rectangle"></div>
             <div className="timezone">
                 <h2>Timezone</h2>
                 <p>UTC -05:00</p>
                 </div>
+                <div className="rectangle"></div>
             <div className="isp">
                 <h2>ISP</h2>
                 <p>SpaceX Starlink</p>
