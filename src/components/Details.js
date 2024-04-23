e2bb9b1152491d103ea9a9abb092d5da5a8b1fa7
CHEGEBB <chegephil24@gmail.com>
2024-04-23 11:08:21 +0300

    js

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,15 +6,19 @@ const DetailsComponent = () => {
         <div className="details container">
             <div className="ip">
                 <h2>Ip Address</h2>
+                <p>192.212.174.101</p>
             </div>
             <div className="location">
                 <h2>Location</h2>
+                <p>Brooklyn, NY 10001</p>
                 </div>
             <div className="timezone">
                 <h2>Timezone</h2>
+                <p>UTC -05:00</p>
                 </div>
             <div className="isp">
                 <h2>ISP</h2>
+                <p>SpaceX Starlink</p>
                 </div>
         </div>
     );
