52e03019d0911510d9edd1973777aeeb9ba47734
CHEGEBB <chegephil24@gmail.com>
2024-04-23 13:03:12 +0300

    scss

--- src/components/Details.js
+++ src/components/Details.js
@@ -20,31 +20,27 @@ const DetailsComponent = ({ ipAddress }) => {
 
     return (  
         <div className="details container">
-            {data && (
-                <>
-                    <div className="ip">
-                        <h2>Ip Address</h2>
-                        <p>{data.ip}</p>
-                    </div>
-                    <div className="rectangle"></div>
-                    <div className="location">
-                        <h2>Location</h2>
-                        <p>{`${data.location.city}, ${data.location.region} ${data.location.postalCode}`}</p>
-                    </div>
-                    <div className="rectangle"></div>
-                    <div className="timezone">
-                        <h2>Timezone</h2>
-                        <p>{data.location.timezone}</p>
-                    </div>
-                    <div className="rectangle"></div>
-                    <div className="isp">
-                        <h2>ISP</h2>
-                        <p>{data.isp}</p>
-                    </div>
-                </>
-            )}
+            <div className="ip">
+                <h2>Ip Address</h2>
+                <p>{data ? data.ip : ''}</p>
+            </div>
+            <div className="rectangle"></div>
+            <div className="location">
+                <h2>Location</h2>
+                <p>{data ? `${data.location.city}, ${data.location.region} ${data.location.postalCode}` : ''}</p>
+            </div>
+            <div className="rectangle"></div>
+            <div className="timezone">
+                <h2>Timezone</h2>
+                <p>UTC{data ? data.location.timezone : ''}</p>
+            </div>
+            <div className="rectangle"></div>
+            <div className="isp">
+                <h2>ISP</h2>
+                <p>{data ? data.isp : ''}</p>
+            </div>
         </div>
     );
 }
- 
+
 export default DetailsComponent;
