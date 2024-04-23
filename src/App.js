85b90f7806db07c703a64259f2b0bdb75a13b936
CHEGEBB <chegephil24@gmail.com>
2024-04-23 10:16:40 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-    <div className="tracker">
-    <div className="header">
-      <h1>Ip Address Tracker</h1>
-      <div className="search">
-      <input type="text" placeholder="Search for any IP address or domain" />
+      <div className="tracker">
+        <div className="header  container-fluid">
+          <h1>Ip Address Tracker</h1>
+          <div className="search">
+            <input type="text" placeholder="Search for any IP address or domain" />
+          </div>
+        </div>
+        <div className="map">
+          <div className="map-container container-fluid">
+            <div id="map"></div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="map">
-      <div className="map-container">
-        <div id="map"></div>
-      </div>
-    </div>
-    </div>
     </div>
   );
 }
